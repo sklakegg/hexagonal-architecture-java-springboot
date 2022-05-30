17d219f5016327a6497356db86e1c5eb2e04d244
Julien Topçu <julien.topcu@gmail.com>
2022-05-30 22:32:52 +0200

    Adding Spring Boot Infrastructure Module

---
+++ infrastructure/src/main/java/rebelsrescue/StarwarsRebelsRescueApplication.java
@@ -0,0 +1,13 @@
+package rebelsrescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsRebelsRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsRebelsRescueApplication.class, args);
+	}
+
+}
