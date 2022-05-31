404056b9c4f53269d35bf1812d73281969a7f3d9
Julien Topçu <julien.topcu@gmail.com>
2022-05-31 18:24:24 +0200

    Implementation of the RescueFleetController

--- infrastructure/src/main/java/rebelsrescue/controllers/RescueFleetController.java
+++ infrastructure/src/main/java/rebelsrescue/controllers/RescueFleetController.java
@@ -1,7 +1,30 @@
 package rebelsrescue.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rebelsrescue.fleet.Fleet;
+import rebelsrescue.fleet.api.AssembleAFleet;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.springframework.http.ResponseEntity.created;
 
 @RestController
+@RequestMapping("/rescueFleets")
 public class RescueFleetController {
+    private AssembleAFleet assembleAFleet;
+
+    public RescueFleetController(AssembleAFleet assembleAFleet) {
+        this.assembleAFleet = assembleAFleet;
+    }
+
+    @PostMapping
+    public ResponseEntity<Fleet> assembleAFleet(@RequestBody RescueFleetRequest rescueFleetRequest) throws URISyntaxException {
+        var fleet = assembleAFleet.forPassengers(rescueFleetRequest.numberOfPassengers);
+        return created(new URI("http://localhost")).body(fleet);
+    }
 }
---
+++ infrastructure/src/main/java/rebelsrescue/controllers/RescueFleetRequest.java
@@ -0,0 +1,5 @@
+package rebelsrescue.controllers;
+
+public class RescueFleetRequest {
+    public Integer numberOfPassengers;
+}
