65721cceb2bcda5ff92730eb9ac473465849cad9
Julien Topçu <julien.topcu@gmail.com>
2022-05-31 11:42:46 +0200

    Adding Missing id in the Fleet

--- domain/src/main/java/rebelsrescue/fleet/Fleet.java
+++ domain/src/main/java/rebelsrescue/fleet/Fleet.java
@@ -1,6 +1,10 @@
 package rebelsrescue.fleet;
 
 import java.util.List;
+import java.util.UUID;
 
-public record Fleet (List<StarShip> starships){
+public record Fleet (UUID id, List<StarShip> starships){
+    public Fleet(List<StarShip> starships) {
+        this(UUID.randomUUID(),starships);
+    }
 }
