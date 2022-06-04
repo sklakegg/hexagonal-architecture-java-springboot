82463e910f17ff2d291f0fac2b25e6720ea89e10
Julien Topçu <julien.topcu@gmail.com>
2022-06-04 21:03:14 +0200

    Make the Application use the Swapi Client instead of the Stub

--- infrastructure/src/main/java/rebelsrescue/configuration/DomainConfiguration.java
+++ infrastructure/src/main/java/rebelsrescue/configuration/DomainConfiguration.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import rebelsrescue.fleet.Fleet;
+import rebelsrescue.fleet.spi.stubs.StarShipInventoryStub;
 
 @Configuration
 @ComponentScan(
         basePackageClasses = {Fleet.class},
-        includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {DomainService.class, Stub.class} )})
+        includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {DomainService.class, Stub.class})},
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {StarShipInventoryStub.class})})
 public class DomainConfiguration {
 }
--- infrastructure/src/test/java/rebelsrescue/controllers/RescueFleetControllerTest.java
+++ infrastructure/src/test/java/rebelsrescue/controllers/RescueFleetControllerTest.java
@@ -1,14 +1,19 @@
 package rebelsrescue.controllers;
 
+import ddd.DomainService;
+import ddd.Stub;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import rebelsrescue.configuration.DomainConfiguration;
-
-import java.util.Objects;
+import rebelsrescue.fleet.Fleet;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,5 +38,11 @@ final class RescueFleetControllerTest {
                 .andExpect(jsonPath("$.starships[0].name").value("Millennium Falcon"))
                 .andExpect(jsonPath("$.starships[0].capacity").value("6"));
     }
+
+    @TestConfiguration
+    @ComponentScan(
+            basePackageClasses = {Fleet.class},
+            includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Stub.class})})
+    static class StubConfiguration{}
 
 }
