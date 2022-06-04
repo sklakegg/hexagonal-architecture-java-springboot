c4858cfa4d3bd3ab31733ad313bfafa0cee2f55c
Julien Topçu <julien.topcu@gmail.com>
2022-06-04 20:07:44 +0200

    Handling non Integer values of passenger in SwapiClient

--- infrastructure/src/main/java/rebelsrescue/swapi/SwapiClient.java
+++ infrastructure/src/main/java/rebelsrescue/swapi/SwapiClient.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import rebelsrescue.fleet.StarShip;
 import rebelsrescue.fleet.spi.StarShipInventory;
+import rebelsrescue.swapi.model.SwapiResponse;
+import rebelsrescue.swapi.model.SwapiStarShip;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static java.lang.Integer.parseInt;
 import static java.util.stream.Collectors.toList;
@@ -28,10 +31,28 @@ public class SwapiClient implements StarShipInventory {
     @Override
     public List<StarShip> starShips() {
         var swapiResponse = getStarShipsFromSwapi();
-        List<StarShip> starShips = swapiResponse.results().stream()
-                .map(swapiStarShip -> new StarShip(swapiStarShip.name(), parseInt(swapiStarShip.passengers())))
+
+        return convertSwapiResponseToStarShips(swapiResponse);
+    }
+
+    private List<StarShip> convertSwapiResponseToStarShips(SwapiResponse swapiResponse) {
+        return swapiResponse.results().stream()
+                .filter(hasValidPassengersValue())
+                .map(toStarShip())
                 .collect(toList());
-        return starShips;
+    }
+
+    private Function<SwapiStarShip, StarShip> toStarShip() {
+        return swapiStarShip ->
+                new StarShip(
+                        swapiStarShip.name(),
+                        parseInt(swapiStarShip.passengers().replaceAll(",", "")));
+    }
+
+    private Predicate<SwapiStarShip> hasValidPassengersValue() {
+        return swapiStarShip -> swapiStarShip.passengers() != null
+                && !swapiStarShip.passengers().equalsIgnoreCase("n/a")
+                && !swapiStarShip.passengers().equalsIgnoreCase("unknown");
     }
 
     private SwapiResponse getStarShipsFromSwapi() {
