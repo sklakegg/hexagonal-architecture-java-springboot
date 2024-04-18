eca0bfcf1ea668dfc06e4ae5cf4e6f5934a62a96
Julien Topçu <julien.topcu@gmail.com>
2024-04-18 15:36:34 +0200

    Handling REST Api deprecations

--- infrastructure/src/main/java/rebelsrescue/controllers/StarShipResource.java
+++ infrastructure/src/main/java/rebelsrescue/controllers/StarShipResource.java
@@ -2,8 +2,14 @@ package rebelsrescue.controllers;
 
 import rebelsrescue.fleet.StarShip;
 
-public record StarShipResource(String name, int capacity) {
+import java.util.Map;
+
+public record StarShipResource(String name, int capacity, int passengersCapacity, Map<String, String> deprecations) {
     public StarShipResource(StarShip starShip) {
-        this(starShip.name(), starShip.passengersCapacity());
+        this(
+                starShip.name(),
+                starShip.passengersCapacity(),
+                starShip.passengersCapacity(),
+                Map.of("capacity", "passengersCapacity"));
     }
 }
